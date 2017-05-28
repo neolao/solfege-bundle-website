ee1fbbbd4da02400f174317f48693123aa89da0b
neolao <contact@neolao.com>
2017-05-28 21:04:01 +0200

    Add server bundle

---
+++ src/Bundle.js
@@ -0,0 +1,29 @@
+/* @flow */
+import ServerBundle from "solfegejs-server"
+import type {ApplicationInterface, BundleInterface, DependentBundleInterface} from "solfegejs/interface"
+
+/**
+ * Website bundle
+ */
+export default class Bundle implements BundleInterface, DependentBundleInterface
+{
+    /**
+     * Get bundle directory
+     *
+     * @return  {string}    Directory path
+     */
+    getPath():string
+    {
+        return  __dirname;
+    }
+
+    /**
+     * Install dependencies
+     *
+     * @param   {ApplicationInterface}  application     SolfegeJS application
+     */
+    *installDependencies(application:ApplicationInterface):Generator<void,void,void>
+    {
+        application.addBundle(new ServerBundle);
+    }
+}
